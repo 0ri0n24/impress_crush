cbbfa61047242c307af0d2e53e60457a17afe2b8
abhishekguleri <abhishekguleri24@gmail.com>
2019-04-08 18:56:58 +0530

    new data added

--- src/Guleri/Main.java
+++ src/Guleri/Main.java
@@ -1,14 +1,26 @@
 package Guleri;
+import java.util.Scanner;
 public class Main {
     public static void main (String[] args) throws java.lang.Exception{
         Scanner in= new Scanner(System.in);
-        int B,n= in.nextInt();
-        int x=0;int[] T = new int[n];
+       int d,r,x=0,B;
+        int n= in.nextInt();
+int[] T = new int[100];
         while( x<n){
             T[x] = in.nextInt();x++;}
-        for (int i:T) {
-            int d = i / 10;
-            int r = i % 10; d = d * 10;
-            if ((r< 6)&&(r>0)) {r= 10;B = d + r;System.out.println(B); }
-            else if((r==0)&&(d>0)){r = 0;B = d + r;System.out.println(B); }
-            else{ r=20; B = d+ r;System.out.println(B);}}}}
+     for(int i:T) {
+         d = (i / 10) * 10;
+         r = i % 10;
+         if ((r < 10) && (d == 0)) {
+             B = 10;
+             System.out.println(B);
+         } else if ((d != 0) && (r < 6)) {
+             B = d + 10;
+             System.out.println(B);
+         } else {
+             B = d + 20;
+             System.out.println(B);
+         }
+
+     }
+     }  }
